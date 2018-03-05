6457c249493926e3f90197bcb7598ad27a6bf5cb
Judah Schwartz <judahsch@gmail.com>
2018-03-05 17:26:53 -0500

    Initial Commit

--- src/PongGui.java
+++ src/PongGui.java
@@ -8,17 +8,18 @@ import java.util.concurrent.ConcurrentHashMap;
 public class PongGui extends JFrame {
     //HELLO WORLD GIT 
     public PongGui(){
-        super();
+
         setTitle("PONG");
         setSize(1500,1000);
         PongPanel pongPanel = new PongPanel();
         add(pongPanel, BorderLayout.CENTER);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
-        pongPanel.setUp();
+
         pongPanel.startBall();
         pongPanel.Paddle(Direction.LEFT);
         pongPanel.Paddle(Direction.RIGHT);
+        pongPanel.requestFocus();
 
     }
 }
@@ -27,141 +28,134 @@ enum Direction{
         }
 class PongPanel extends JPanel //implements Runnable
 {
-    Graphics2D g;
-    Set<Character> keysPressed = new ConcurrentHashMap<Character,Object>().newKeySet();
+    Set<Character> keysPressed = new ConcurrentHashMap<Character, Object>().newKeySet();
     int lPos = 450;
     int rPos = 450;
+    int paddleSpeed = 4;
 
     PongPanel() {
         System.out.println();
         setBackground(Color.BLACK);
-
-
-    }
-    public void setUp()
-    {
-        g = (Graphics2D) getGraphics();
-        requestFocus();
-
+        addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                if (e.getPreciseWheelRotation() > 0 && lPos >= 10 + paddleSpeed)
+                    lPos -= paddleSpeed;
+                if (e.getPreciseWheelRotation() < 0 && lPos <= 710 - paddleSpeed) {
+                    lPos += paddleSpeed;
+                }
+            }
+        });
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-
-
-                keysPressed.add( e.getKeyChar());
-
+                keysPressed.add(e.getKeyChar());
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
                 keysPressed.remove(e.getKeyChar());
-
             }
         });
     }
-    public void changePosValues()
-    {
-        if(keysPressed.contains('w'))
-        {
 
-            if(lPos >=14)
-                lPos-=4;
+    public void changePosValues() {
+//        if(keysPressed.contains('w'))
+//        {
+//
+//            if(lPos >=14)
+//                lPos-=paddleSpeed;
+//        }
+//        if(keysPressed.contains('s'))
+//        {
+//          if(lPos <= 706)
+//              lPos+=paddleSpeed;
+//        }
+        if (keysPressed.contains('i')) {
+            if (rPos >= 10 + paddleSpeed)
+                rPos -= paddleSpeed;
         }
-        if(keysPressed.contains('s'))
-        {
-            if(lPos <= 706)
-            lPos+=4;
-        }
-        if(keysPressed.contains('i'))
-        {
-            if(rPos>=14)
-            rPos-=4;
-        }
-        if(keysPressed.contains('k'))
-        {
-            if(rPos <= 706)
-            rPos+=4;
+        if (keysPressed.contains('k')) {
+            if (rPos <= 710 - paddleSpeed)
+                rPos += paddleSpeed;
         }
     }
-    public void Paddle(Direction d)
-    {
+
+    public void Paddle(Direction d) {
+        Graphics2D g = (Graphics2D) getGraphics();
 
         Timer timer = new Timer(40, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
                 changePosValues();
-                synchronized (g) {
-                    g.setColor(Color.black);
 
-
-
-                    switch (d) {
-                        case LEFT:
-                            g.fillRect(100,10,50,900);
-                            g.setColor(Color.white);
-                            g.fillRoundRect(100, lPos, 50, 200, 10, 10);
-                            break;
-                        case RIGHT:
-                            g.fillRect(1300,10,50,900);
-                            g.setColor(Color.white);
-                            g.fillRoundRect(1300, rPos, 50, 200, 10, 10);
-                           // System.out.println("hello from right timer");
-                            break;
-                    }
+                g.setColor(Color.black);
+                switch (d) {
+                    case LEFT:
+                        g.fillRect(100, 10, 50, 900);
+                        g.setColor(Color.white);
+                        g.fillRoundRect(100, lPos, 50, 200, 10, 10);
+                        break;
+                    case RIGHT:
+                        g.fillRect(1300, 10, 50, 900);
+                        g.setColor(Color.white);
+                        g.fillRoundRect(1300, rPos, 50, 200, 10, 10);
+                        // System.out.println("hello from right timer");
+                        break;
                 }
             }
         });
         timer.start();
-
     }
-    public void startBall()
-    {
 
+    public void startBall() {
+        Graphics2D g = (Graphics2D) getGraphics();
         Timer timer = new Timer(40, new ActionListener() {
-            int x = 200,  y = 500;
+            int x = 200, y = 500;
             int dx = 10, dy = 10;
+            int ballRadius = 25;
+
             @Override
             public void actionPerformed(ActionEvent e) {
-               synchronized (g) {
-                   g.setColor(Color.black);
-                   g.fillOval(x, y, 50, 50);
-                   g.setColor(Color.white);
-                   x += dx;
-                   y += dy;
-                   g.fillOval(x, y, 50, 50);
-                   g.drawRect(10,9,1450,902);
-               }
-                if(y <= 10 || y >= 850)
+
+                g.setColor(Color.black);
+                g.fillOval(x, y, 2 * ballRadius, 2 * ballRadius);
+                g.setColor(Color.white);
+                x += dx;
+                y += dy;
+                g.fillOval(x, y, 2 * ballRadius, 2 * ballRadius);
+                g.drawRect(10, 9, 1450, 902);
+                //keep ball bouncing in frame
+                if (y <= 10 || y >= 900
+                        - 2 * ballRadius)//to accomodate for the height of the ball
                 {
+
                     dy *= -1;
                 }
-                if(x == 150 )
-                {
-                    if((y + 25) - lPos < 225 &&(y + 25) - lPos > -25)
+                if (x == 150) {
+                    if ((y + ballRadius) - lPos < 225
+                            && (y + ballRadius) - lPos > -ballRadius)//ball is not within range of the left paddle
                     {
-                        dx*=-1;
+                        dx *= -1;
                     }
                 }
 
-                if(x == 1250)
-                {
-                    if((y + 25) - rPos < 225 &&(y + 25) - rPos > -25)
+                if (x == 1250) {
+                    if ((y + ballRadius) - rPos < 225
+                            && (y + ballRadius) - rPos > -ballRadius)//ball is not within range of the right paddle
                     {
-                        dx*=-1;
+                        dx *= -1;
                     }
                 }
-                if(x < -50 || x > 1500)
+                if (x < - (2 * ballRadius) || x > 1500)//the ball went out of bounds
                 {
-                   JOptionPane.showMessageDialog(PongPanel.this,"Game Over");
-                   System.exit(0);
+                    JOptionPane.showMessageDialog(PongPanel.this, "Game Over");
+                    System.exit(0);
                 }
-
             }
         });
 
         timer.start();
     }
-
-
 }
